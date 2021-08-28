85a3cf92029c7dfef387f58c5661f5176a118f6a
Titanium <88758453+Titanium2099@users.noreply.github.com>
2021-08-27 19:38:23 -0700

    Update timeago.js
    added **:** (am/pm)

--- timeago.js
+++ timeago.js
@@ -1,21 +1,28 @@
 // structure - (* second(s) ago (1 -59)) - (* minute(s) ago(1-45)) - (about an hour ago (46-60 minutes)) - (* hour(s) ago (1-16)) - (about a day ago (17-23 hours)) - (* day(s) ago(1-6)) - about  a week ago ----- etc  
 var uniqueTime = [] //right now and yesterday at *:** (am/pm)
-var relativeTime = [
+var aTime = [
   ["minute", "hour", "day", "week", "month", "year"],
   [60000, 3600000, 86400000, 604800000, 2629800000, 31557600000],
   [], "about a ", " ago"
 ]; //sorted in [[text],[minimum value],[maximum value], generic text] value in milliseconds
+var multiTime = [] //
 function timeago(e) {
-  console.log(Date.now() - e.TimeStamp)
-  for (var aa = 0; aa < relativeTime[1].length; aa++) {
-    if ((relativeTime[1][(relativeTime[1].length - (aa + 1))] - 1) < (Date.now() - e.TimeStamp)) {
-      console.log(relativeTime[3] + relativeTime[0][relativeTime[1].length - (aa + 1)] + relativeTime[4])
+  var currDate = new Date();
+var oldDate = new Date(e.TimeStamp);
+if((currDate.getDate() == oldDate.getDate())&& ((Date.now() - e.TimeStamp) < 86400001)){
+    //today at **:** (am/pm)
+  function apm(){if(currDate.getHours()>11){return 'pm';}else{return 'am';}};
+return "Today at "+(((currDate.getHours() - 12)*(currDate.getHours()>12))+((currDate.getHours())*(currDate.getHours()<13))) + ':' + currDate.getMinutes() + apm();
+}else{
+  for (var aa = 0; aa < aTime[1].length; aa++) {
+    if ((aTime[1][(aTime[1].length - (aa + 1))] - 1) < (Date.now() - e.TimeStamp)) {
+      return aTime[3] + aTime[0][aTime[1].length - (aa + 1)] + aTime[4]
       break;
     }
   }
-
 }
-timeago({
+}
+console.log(timeago({
   "UTC": true,
-  "TimeStamp": (Date.now() - 70000)
-})
+  "TimeStamp": (Date.now() - 700000000)
+}))
