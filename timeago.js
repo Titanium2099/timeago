4f5719596ca42586f0e164619ff70018bafc5b8e
Titanium <88758453+Titanium2099@users.noreply.github.com>
2021-08-28 22:51:37 -0700

    Update timeago.js

--- timeago.js
+++ timeago.js
@@ -1,12 +1,10 @@
 // structure - (* second(s) ago (1 -59)) - (* minute(s) ago(1-45)) - (about an hour ago (46-60 minutes)) - (* hour(s) ago (1-16)) - (about a day ago (17-23 hours)) - (* day(s) ago(1-6)) - about  a week ago ----- etc  
-var uniqueTime = [] //right now and yesterday at *:** (am/pm)
-var aTime = [
+function timeago(e) {
+  var aTime = [
   ["minute", "hour", "day", "week", "month", "year"],
   [60000, 3600000, 86400000, 604800000, 2629800000, 31557600000],
   [], "about a ", " ago"
 ]; //sorted in [[text],[minimum value],[maximum value], generic text] value in milliseconds
-var multiTime = [] //
-function timeago(e) {
   if((Date.now() - e.TimeStamp) < 60000){
   return 'right now';
 }
